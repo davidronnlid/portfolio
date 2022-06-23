073a34afa650b2ffa33df2196f1c52cc932e7c53
David Rönnlid <david.ronnlid@gmail.com>
2022-06-23 19:01:20 +0200

    Header almost completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import ResponsiveAppBar from "./components/header"
+
 
 function App() {
   return (
     <div className="App">
+      <ResponsiveAppBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
