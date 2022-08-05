8ab9811fb349f206ef1e9e325164f0fc92d07015
David Rönnlid <david.ronnlid@gmail.com>
2022-08-05 21:18:09 +0200

    Phrasing upgrade in "Why you should hire me" sect.

--- src/homePage/homePageSections/whyMe.tsx
+++ src/homePage/homePageSections/whyMe.tsx
@@ -13,18 +13,18 @@ const WhyMe: React.FC<WhyMeProps> = ({ title }: WhyMeProps): JSX.Element => {
             <ul>
                 <li style={{ marginBottom: '1rem' }}>
                     <Typography variant="body1">
-                        I love working with people who are driven to learn
+                        I love to work with people who are driven to learn
                     </Typography>
                 </li>
                 <li style={{ marginBottom: '1rem' }}>
                     <Typography variant="body1">
-                        I love creating great products with code
+                        I love to create great products with code
                     </Typography>
                 </li>
                 <li style={{ marginBottom: '1rem' }}>
                     <Typography variant="body1">
-                        I am a passionate junior front-end developer specialized
-                        in React and TypeScript
+                        I am a passionate junior front-end web developer
+                        specialized in React and TypeScript
                     </Typography>
                 </li>
             </ul>
